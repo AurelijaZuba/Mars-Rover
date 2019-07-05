fe815c384e51f0ea391020b93a021f21c5f9b0b8
AurelijaZuba <auryte00@hotmail.com>
2019-07-05 12:34:46 +0100

    Refactoring ENUM class

--- src/main/java/com.codurance/Compass.java
+++ src/main/java/com.codurance/Compass.java
@@ -5,10 +5,28 @@ public enum Compass {
     private String value;
 
     public String getValue() {
+
         return value;
     }
 
+    public static Compass convertFromString(String values) {
+        if (values.equals("N")) {
+            return Compass.NORTH;
+        }
+        if (values.equals("E")) {
+            return Compass.EAST;
+        }
+        if (values.equals("S")) {
+            return Compass.SOUTH;
+        }
+        if (values.equals("W")) {
+            return Compass.WEST;
+        }
+        return null;
+    }
+
     Compass(String value) {
+
         this.value = value;
     }
 }
--- src/main/java/com.codurance/MarsRover.java
+++ src/main/java/com.codurance/MarsRover.java
@@ -2,17 +2,17 @@ package com.codurance;
 
 public class MarsRover {
     private final int[] gridCoordinates;
-    private String direction;
+    private Compass direction;
 
     public MarsRover(int[] gridCoordinates, String direction) {
         this.gridCoordinates = gridCoordinates;
-        this.direction = direction;
+        this.direction = Compass.convertFromString(direction);
     }
 
     public String execute(String command) {
         handleInstructions(command);
 
-        return gridCoordinates[0] + ", " + gridCoordinates[1] + ", " + direction;
+        return gridCoordinates[0] + ", " + gridCoordinates[1] + ", " + direction.getValue();
     }
 
     private void handleInstructions(String command) {
@@ -22,25 +22,25 @@ public class MarsRover {
             if (instruction == 'R')
                 rotateRight();
             if (instruction == 'L')
-                direction = Compass.WEST.getValue();
+                direction = Compass.WEST;
         }
     }
 
     private void rotateRight() {
-        if(direction.equals(Compass.NORTH.getValue())) {
-            direction = Compass.EAST.getValue();
+        if(direction.equals(Compass.NORTH)) {
+            direction = Compass.EAST;
             return;
         }
-        if(direction.equals(Compass.EAST.getValue())) {
-            direction = Compass.SOUTH.getValue();
+        if(direction.equals(Compass.EAST)) {
+            direction = Compass.SOUTH;
             return;
         }
-        if(direction.equals(Compass.SOUTH.getValue())) {
-            direction = Compass.WEST.getValue();
+        if(direction.equals(Compass.SOUTH)) {
+            direction = Compass.WEST;
             return;
         }
-        if(direction.equals(Compass.WEST.getValue())) {
-            direction = Compass.NORTH.getValue();
+        if(direction.equals(Compass.WEST)) {
+            direction = Compass.NORTH;
             return;
         }
     }
