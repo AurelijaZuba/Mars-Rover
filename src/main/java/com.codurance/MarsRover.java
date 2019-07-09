d35c6a5956c9d936d4cfa72716730af35bead4e2
AurelijaZuba <auryte00@hotmail.com>
2019-07-09 10:50:03 +0100

    Refactoring the state of rover - extract methods and move to their respected classes

--- src/main/java/com.codurance/MarsRover.java
+++ src/main/java/com.codurance/MarsRover.java
@@ -1,63 +1,31 @@
 package com.codurance;
 
 public class MarsRover {
-    private final int[] gridCoordinates;
-    private Compass direction;
+    private final State state;
 
     public MarsRover(int[] gridCoordinates, String direction) {
-        this.gridCoordinates = gridCoordinates;
-        this.direction = Compass.convertFromString(direction);
+        state = new State(gridCoordinates, Compass.convertFromString(direction));
     }
 
     public String execute(String command) {
         handleInstructions(command);
 
-        return gridCoordinates[0] + ", " + gridCoordinates[1] + ", " + direction.getValue();
+        return state.toString();
     }
 
     private void handleInstructions(String command) {
         char[] split = command.toCharArray();
 
         for (char instruction : split) {
-            if (instruction == 'R')
-                rotateRight();
-            if (instruction == 'L') {
-                if(direction.equals(Compass.NORTH)) {
-                    direction = Compass.WEST;
-                    continue;
-                }
-                if(direction.equals(Compass.WEST)) {
-                    direction = Compass.SOUTH;
-                    continue;
-                }
-                if(direction.equals(Compass.SOUTH)) {
-                    direction = Compass.EAST;
-                    continue;
-                }
-                if(direction.equals(Compass.EAST)) {
-                    direction = Compass.NORTH;
-                    continue;
-                }
-            }
+            executeCommand(instruction);
         }
     }
 
-    private void rotateRight() {
-        if(direction.equals(Compass.NORTH)) {
-            direction = Compass.EAST;
-            return;
-        }
-        if(direction.equals(Compass.EAST)) {
-            direction = Compass.SOUTH;
-            return;
-        }
-        if(direction.equals(Compass.SOUTH)) {
-            direction = Compass.WEST;
-            return;
-        }
-        if(direction.equals(Compass.WEST)) {
-            direction = Compass.NORTH;
-            return;
+    private void executeCommand(char instruction) {
+        if (instruction == 'R')
+            state.rotateRight();
+        if (instruction == 'L') {
+            state.rotateLeft();
         }
     }
 }
---
+++ src/main/java/com.codurance/State.java
@@ -0,0 +1,53 @@
+package com.codurance;
+
+public class State {
+    private final int[] gridCoordinates;
+
+    private Compass direction;
+
+    public State(int[] gridCoordinates, Compass direction) {
+        this.gridCoordinates = gridCoordinates;
+        this.direction = direction;
+    }
+
+    @Override
+    public String toString() {
+        return gridCoordinates[0] + ", " + gridCoordinates[1] + ", " + direction.getValue();
+    }
+
+    void rotateLeft() {
+        if(direction.equals(Compass.NORTH)) {
+            this.direction = Compass.WEST;
+            return;
+        }
+        if(direction.equals(Compass.WEST)) {
+            this.direction = Compass.SOUTH;
+            return;
+        }
+        if(direction.equals(Compass.SOUTH)) {
+            this.direction = Compass.EAST;
+            return;
+        }
+        if(direction.equals(Compass.EAST)) {
+            this.direction = Compass.NORTH;
+        }
+    }
+
+    void rotateRight() {
+        if(direction.equals(Compass.NORTH)) {
+            this.direction = Compass.EAST;
+            return;
+        }
+        if(direction.equals(Compass.EAST)) {
+            this.direction = Compass.SOUTH;
+            return;
+        }
+        if(direction.equals(Compass.SOUTH)) {
+            this.direction = Compass.WEST;
+            return;
+        }
+        if(direction.equals(Compass.WEST)) {
+            this.direction = Compass.NORTH;
+        }
+    }
+}
